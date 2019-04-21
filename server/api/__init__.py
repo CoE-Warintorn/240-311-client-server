340ec41d78d90e2a1bb36f5cf997df0c241747a0
Jakee Indapanya <ji@Groot>
2019-04-21 20:38:04 +0700

    app config

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -4,3 +4,8 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     app.config.from_object(setting)
+    app.config.from_envvar('BOOK_SETTINGS')
+
+    cors = CORS(app, resource={r'/api/*': {'origin': '*'}})
+
+    return app
