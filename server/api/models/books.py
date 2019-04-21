ca69f7194c5bea05331b6f6a7f697ec7b526c44b
Jakee Indapanya <ji@Groot>
2019-04-21 21:52:13 +0700

    add to_dict to books models

--- server/api/models/books.py
+++ server/api/models/books.py
@@ -2,12 +2,18 @@ import mongoengine as me
 import datetime
 
 class Book(me.Document):
-    title = me.StringField()
-    author = me.StringField()
-    isRead = me.BooleanField()
+    title = me.StringField(required=True)
+    author = me.StringField(required=True)
+    is_read = me.BooleanField(required=True)
     created_time = me.DateTimeField(required=True,
                                     default=datetime.datetime.now)
     updated_time = me.DateTimeField(required=True,
                                     default=datetime.datetime.now)
 
     meta = {'collection': 'books'}
+
+    def to_dict(self):
+        return dict(id=str(self.id),
+                    title=self.title,
+                    author=self.author,
+                    is_read=self.is_read)
