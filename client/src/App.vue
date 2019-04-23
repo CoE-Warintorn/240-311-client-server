48ed7e27a441a522ce28e15efa67cfcd44709f78
Jakee Indapanya <ji@Groot>
2019-04-23 21:33:39 +0700

    Books page

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,22 +2,20 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Books</span>
+        <!-- <span class="font-weight-light">MATERIAL DESIGN</span> -->
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/essec/240-311-client-server"
         target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Repository</span>
       </v-btn>
     </v-toolbar>
-
     <v-content>
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
-
