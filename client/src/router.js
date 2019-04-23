c81c60be37991669055e1218c2f54ac85de3ab0d
Jakee Indapanya <ji@Groot>
2019-04-23 21:33:21 +0700

    routerOption

--- client/src/router.js
+++ client/src/router.js
@@ -1,24 +1,24 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+// import Home from './views/Home.vue';
+
+const routerOption = [
+  {
+    path: '/',
+    component: 'Books',
+    name: 'Books',
+  },
+];
+
+// eslint-disable-next-line no-undef
+const routes = routerOption.map(route => ({
+  ...route,
+  component: () => import(`@/views/${route.component}.vue`),
+}));
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
+  routes,
   mode: 'history',
 });
