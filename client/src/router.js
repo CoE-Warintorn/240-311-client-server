bc6063b68f234403570bf093701624709102978d
Jakee Indapanya <ji@Groot>
2019-04-25 22:12:52 +0700

    change css to bootstrap

--- client/src/router.js
+++ client/src/router.js
@@ -1,16 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-// import Home from './views/Home.vue';
 
 const routerOption = [
   {
     path: '/',
-    component: 'Books',
-    name: 'Books',
+    name: 'Home',
+    component: 'Home',
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: 'About',
   },
 ];
 
-// eslint-disable-next-line no-undef
 const routes = routerOption.map(route => ({
   ...route,
   component: () => import(`@/views/${route.component}.vue`),
@@ -19,6 +22,7 @@ const routes = routerOption.map(route => ({
 Vue.use(Router);
 
 export default new Router({
-  routes,
   mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
 });
