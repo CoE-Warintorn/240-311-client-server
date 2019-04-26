ff494159f887b79a9d3cb0a321d3aa4c095b04cc
Jakee Indapanya <ji@Groot>
2019-04-26 16:44:43 +0700

    add setup.py

---
+++ server/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+f = open('requirements.txt', 'r')
+
+requires = f.read().split('\n')
+
+f.close()
+
+setup(name='api',
+      version='0.0.1',
+      description='',
+      install_requires=requires,
+      packages=find_packages(),
+      include_package_data=True,
+      author='',
+      author_email='',
+      url='',
+     )
